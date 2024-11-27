c39a1b862c34be4b472e7b9e8beb3ba8db3db5ce
bhimrazy <bhimrajyadav977@gmail.com>
2024-11-28 01:19:48 +0545

    configure OpenAI client with custom base URL and API key; print response from embeddings creation

--- embeddings-api/client.py
+++ embeddings-api/client.py
@@ -1,9 +1,14 @@
 from openai import OpenAI
 
-client = OpenAI()
+client = OpenAI(
+    base_url="http://127.0.0.1:8000/v1",
+    api_key="lit",
+)
 
-client.embeddings.create(
+response = client.embeddings.create(
     model="text-embedding-ada-002",
     input="The food was delicious and the waiter...",
     encoding_format="float",
 )
+
+print(response)
