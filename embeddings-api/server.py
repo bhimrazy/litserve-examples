534ab3fb76183e51faf7ca544672d004c70222bb
bhimrazy <bhimrajyadav977@gmail.com>
2024-12-06 14:20:50 +0545

    updates litserver file

--- embeddings-api/server.py
+++ embeddings-api/server.py
@@ -4,7 +4,9 @@ from litserve import LitAPI, LitServer, OpenAIEmbeddingSpec
 
 class EmbeddingAPI(LitAPI):
     def setup(self, device):
-        self.model = TextEmbedding("jinaai/jina-embeddings-v2-small-en")
+        self.model = TextEmbedding(
+            "jinaai/jina-embeddings-v2-small-en"
+        )  # 512 dim, 0.120 GB
 
     def predict(self, documents):
         return list(self.model.embed(documents))
@@ -12,32 +14,5 @@ class EmbeddingAPI(LitAPI):
 
 if __name__ == "__main__":
     api = EmbeddingAPI()
-    server = LitServer(api, spec=OpenAIEmbeddingSpec())
+    server = LitServer(api, spec=OpenAIEmbeddingSpec())  # , workers_per_device=2)
     server.run(port=8000)
-
-
-# curl http://localhost:8000/v1/embeddings \
-#   -H "Content-Type: application/json" \
-#   -d '{
-#     "input": "A beautiful sunset over the beach",
-#     "model": "jinaai/jina-embeddings-v2-small-en",
-#     "encoding_format": "float"
-#   }'
-
-# Output:
-# {
-#     "data": [
-#         {
-#             "index": 0,
-#             "embedding": [
-#                 0.05936763063073158,
-#                 ...,
-#                 0.01047902274876833,
-#             ],
-#             "object": "embedding",
-#         }
-#     ],
-#     "model": "jinaai/jina-embeddings-v2-small-en",
-#     "object": "list",
-#     "usage": {"prompt_tokens": 0, "total_tokens": 0},
-# }
