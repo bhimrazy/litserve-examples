d55bf3ad71aa069bc45e8ff029bb075dba1c339d
bhimrazy <bhimrajyadav977@gmail.com>
2024-12-07 16:53:18 +0545

    fix: update server port to 8000 in EmbeddingAPI

--- embeddings-api/server.py
+++ embeddings-api/server.py
@@ -18,5 +18,5 @@ class EmbeddingAPI(LitAPI):
 
 if __name__ == "__main__":
     api = EmbeddingAPI()
-    server = LitServer(api, spec=OpenAIEmbeddingSpec())  # , workers_per_device=2)
+    server = LitServer(api, spec=OpenAIEmbeddingSpec())
     server.run(port=8000, generate_client_file=False)
